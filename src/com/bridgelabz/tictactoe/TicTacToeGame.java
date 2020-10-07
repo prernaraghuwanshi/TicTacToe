c74d55c97164f1a0e410a44f77d6c81716f5cad0
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-07 10:36:30 +0530

    [Prerna]ADDED: Method to display the board

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -4,28 +4,39 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	public static char[] board;
 	static Scanner sc = new Scanner(System.in);
-	public static char PLAYER;
-	public static char COMPUTER;
+	
 	//Method to create board
 	public static char[] createBoard() {
-		board = new char[10];
+		char[] board = new char[10];
 		Arrays.fill(board, ' ');
 		return board;
-	}
-	
+	}	
 	//Method to allow player to choose character
 	public static char chooseCharacterForPlayer() {
 		System.out.println("Choose character: 'X' or 'O' ");
 		char userChar = sc.nextLine().charAt(0);
 		return userChar;
+	}	
+	//Method to show board
+	public static void showBoard(char[] board)
+	{
+		for (int position = 1; position < 10; position++) {
+			System.out.print(board[position]);
+			if (position % 3 == 0)
+				System.out.println("");
+			else
+				System.out.print(" | ");
+		}
 	}
 
 	public static void main(String args[]) {
 		char[] board = createBoard();
-		PLAYER = chooseCharacterForPlayer();
-		System.out.println(PLAYER);
+		char player = chooseCharacterForPlayer();
+		char computer = (player == 'X') ? 'O' : 'X';
+		System.out.println("Player chooses: "+ player);
+		System.out.println("Computer character: "+ computer);
+		showBoard(board);
 	}
 
 }
