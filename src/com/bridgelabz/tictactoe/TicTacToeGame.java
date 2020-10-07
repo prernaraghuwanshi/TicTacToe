c44b2e10f33a071cca265064df18d3fe554c7472
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-07 11:29:34 +0530

    [Prerna]ADDED: Function for user to make a move

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -4,23 +4,22 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	static Scanner sc = new Scanner(System.in);
-	
-	//Method to create board
+	// Method to create board
 	public static char[] createBoard() {
 		char[] board = new char[10];
 		Arrays.fill(board, ' ');
 		return board;
-	}	
-	//Method to allow player to choose character
-	public static char chooseCharacterForPlayer() {
+	}
+
+	// Method to allow player to choose character
+	public static char chooseCharacterForPlayer(Scanner scannerObj) {
 		System.out.println("Choose character: 'X' or 'O' ");
-		char userChar = sc.nextLine().charAt(0);
-		return userChar;
-	}	
-	//Method to show board
-	public static void showBoard(char[] board)
-	{
+		return scannerObj.next().toUpperCase().charAt(0);
+	}
+
+	// Method to show board
+	public static void showBoard(char[] board) {
+		System.out.println("Board:");
 		for (int position = 1; position < 10; position++) {
 			System.out.print(board[position]);
 			if (position % 3 == 0)
@@ -30,13 +29,27 @@ public class TicTacToeGame {
 		}
 	}
 
-	public static void main(String args[]) {
-		char[] board = createBoard();
-		char player = chooseCharacterForPlayer();
-		char computer = (player == 'X') ? 'O' : 'X';
-		System.out.println("Player chooses: "+ player);
-		System.out.println("Computer character: "+ computer);
+	// Method for user to make a move
+	public static void userMove(Scanner scannerObj, char[] board, char player) {
+		System.out.println("Make your move.\nEnter index ( 1 to 9)");
+		int index = scannerObj.nextInt();
+		if (board[index] != ' ') {
+			System.out.println("Board position is not empty. Try again!");
+			userMove(scannerObj, board, player);
+		}
+		board[index] = player;
 		showBoard(board);
+	}
+
+	public static void main(String args[]) {
+		Scanner scannerObj = new Scanner(System.in);
+		char[] board = createBoard();
+		char player = chooseCharacterForPlayer(scannerObj);
+		char computer = (player == 'X') ? 'O' : 'X';
+		System.out.println("Player chooses: " + player);
+		System.out.println("Computer character: " + computer);
+		showBoard(board);
+		userMove(scannerObj, board, player);
 	}
 
 }
