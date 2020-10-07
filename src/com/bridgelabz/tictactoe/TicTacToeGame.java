d0afc90e6ab41b684972c00557dd362ed3fc527c
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-07 09:53:40 +0530

    [Prerna]ADDED: Function for player to choose character

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,20 +1,31 @@
 package com.bridgelabz.tictactoe;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToeGame {
-	
 	public static char[] board;
-	
-	public static void createBoard()
-	{
+	static Scanner sc = new Scanner(System.in);
+	public static char PLAYER;
+	public static char COMPUTER;
+	//Method to create board
+	public static char[] createBoard() {
 		board = new char[10];
 		Arrays.fill(board, ' ');
-		
+		return board;
 	}
-	public static void main(String args[]) 
-	{
-		createBoard();
+	
+	//Method to allow player to choose character
+	public static char chooseCharacterForPlayer() {
+		System.out.println("Choose character: 'X' or 'O' ");
+		char userChar = sc.nextLine().charAt(0);
+		return userChar;
+	}
+
+	public static void main(String args[]) {
+		char[] board = createBoard();
+		PLAYER = chooseCharacterForPlayer();
+		System.out.println(PLAYER);
 	}
 
 }
